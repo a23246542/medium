c7cf5e921d399ed032db8a822c9bf79927cfb881
a23246542 <a23246542@gmail.com>
2020-11-04 23:20:01 +0800

    actions跟types拆分管理

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,15 +1,16 @@
+import * as actionTypes from './actionTypes';
 const defaultState = {
   focused:false,
 }
 //純函數
 export default (state=defaultState,action) => {
   switch (action.type) {
-    case 'searchFocus':{
+    case actionTypes.SEARCH_FOCUS:{
       return {
         focused:true,
       }
     }
-    case 'searchBlur':{
+    case actionTypes.SEARCH_BLUR:{
       return {
         focused:false,
       }
