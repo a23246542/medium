61a3bfb73776fd105bf3f7a2b9cefefbacc2c987
sam.yang <sam.yang@itnsl.net>
2020-11-06 17:58:22 +0800

    改express +mock.js模擬數據

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,16 @@
 import axios from 'axios';
 
 let api = axios.create({
-  // baseURL: 'http://localhost:3000',
+  baseURL: 'http://localhost:5000',
   headers: { 'Content-Type': 'application/json' },
 })
 
-api.defaults.timeout = 2500;
+api.defaults.timeout = 10500;
 
 
 export default {
   getHotSearchList(){
-    return api.get('/api/headerList.json');
+    // return api.get('/api/headerList.json');
+    return api.get('/headerList');//@@直接連代理都不用 後端開啟跨域
   }
 }
---
+++ src/api/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const Mock = require('mockjs');
+// const proxy = require('http-proxy-middleware')
+const app = express();
+const headersList = require('./data/headerList');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  // 此处根据前端请求携带的请求头进行配置
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+  // 例如： 我们公司的请求头需要携带Authorization和Client-Type，此处就应该按照以下进行配置
+  // res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization, Client-Type");
+  next();
+})
+
+
+app.get('/headerList',(req,res) => {
+  // res.json({
+    //   success: true,
+    //   data: ["高考","区块链","三生三世","崔永元","vue","小程序","茶点微小说","萨沙讲史堂","夜幕下的地安门","擦亮你的眼","理财","毕业","手帐","简书交友","spring","古风","故事","暖寄归人","旅行","连载","教育","简书","生活","投稿","历史","PHP","考研","docker","EOS","微信小程序","PPT","职场","大数据","创业","书评","东凤","饱醉豚","雨落荒原","程序员","爬虫","时间管理","kotlin","数据分析","阴阳合同","设计","红楼梦","父亲节","女人和衣服","swift","高考作文"]
+    // })
+    setTimeout(
+      function(){
+
+        res.json(Mock.mock(headersList))
+      },5000
+    )
+  })
+
+  // Mock.setUp();
+
+app.listen(5000,()=>{
+  console.log('监听端口');
+
+});
