eb9ec153692c8353a86f26e7f67679c5b40c3752
sam.yang <sam.yang@itnsl.net>
2020-11-10 10:18:49 +0800

    主題區塊布局

--- src/pages/home/components/Topic.js
+++ src/pages/home/components/Topic.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from 'react';
+import {TopicWrapper, TopicItem} from '../style';
 
 const Topic = () => {
   return (
-    <div>
-
-    </div>
+    <TopicWrapper>
+      <TopicItem>
+        <img
+          className="topicItem__pic"
+          src="//upload.jianshu.io/collections/images/261938/man-hands-reading-boy-large.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64"
+          alt=""
+        />
+        社会热点
+      </TopicItem>
+      <TopicItem>
+        <img
+          className="topicItem__pic"
+          src="//upload.jianshu.io/collections/images/261938/man-hands-reading-boy-large.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64"
+          alt=""
+        />
+        社会热点
+      </TopicItem>
+    </TopicWrapper>
   )
 }
 
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
 
 export const HomeWrapper = styled.div`
   overflow: hidden;
@@ -21,4 +21,32 @@ export const HomeLeft = styled.div`
 export const HomeRight = styled.div`
 	width: 280px;
 	float: right;
+`;
+
+export const TopicWrapper = styled.div`
+	overflow: hidden;
+	padding: 20px 0 10px 0;
+	margin-left: -18px;
+	border-bottom: 1px solid #dcdcdc;
+`;
+
+export const TopicItem = styled.div`
+	float: left;
+	height: 32px;
+	line-height: 32px;
+	margin-left: 18px;
+	margin-bottom: 18px;
+	padding-right: 10px;
+	background: #f7f7f7;
+	font-size: 14px;
+	color: #000;
+	border: 1px solid #dcdcdc;
+	border-radius: 4px;
+	.topicItem__pic {
+		display: block;
+		float: left;
+		width: 32px;
+		height: 32px;
+		margin-right: 10px;
+	}
 `;
