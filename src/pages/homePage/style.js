47fb925d9355390767896182dbb5d9965a48d125
a23246542 <a23246542@gmail.com>
2021-04-14 21:49:18 +0800

    fix: stylelint舊版更新

--- src/pages/homePage/style.js
+++ src/pages/homePage/style.js
@@ -8,53 +8,56 @@ export const HomeWrapper = styled.div`
 
 
 export const HomeLeft = styled.div`
-	float: left;
-	margin-left: 15px;
-	padding-top: 30px;
-	width: 625px;
-	.banner-img {
-		width: 100%;
-		height: 270px;
-	}
+  float: left;
+  margin-left: 15px;
+  padding-top: 30px;
+  width: 625px;
+
+  .banner-img {
+    width: 100%;
+    height: 270px;
+  }
 `;
 
 export const HomeRight = styled.div`
-	width: 280px;
-	float: right;
+  width: 280px;
+  float: right;
 `;
 
 export const TopicWrapper = styled.div`
-	overflow: hidden;
-	padding: 20px 0 10px 0;
-	margin-left: -18px;
-	border-bottom: 1px solid #dcdcdc;
+  overflow: hidden;
+  padding: 20px 0 10px 0;
+  margin-left: -18px;
+  border-bottom: 1px solid #dcdcdc;
 `;
 
 export const TopicItem = styled.div`
-	float: left;
-	height: 32px;
-	line-height: 32px;
-	margin-left: 18px;
-	margin-bottom: 18px;
-	padding-right: 10px;
-	background: #f7f7f7;
-	font-size: 14px;
-	color: #000;
-	border: 1px solid #dcdcdc;
-	border-radius: 4px;
-	.topicItem__pic {
-		display: block;
-		float: left;
-		width: 32px;
-		height: 32px;
-		margin-right: 10px;
-	}
+  float: left;
+  height: 32px;
+  line-height: 32px;
+  margin-left: 18px;
+  margin-bottom: 18px;
+  padding-right: 10px;
+  background: #f7f7f7;
+  font-size: 14px;
+  color: #000;
+  border: 1px solid #dcdcdc;
+  border-radius: 4px;
+
+  .topicItem__pic {
+    display: block;
+    float: left;
+    width: 32px;
+    height: 32px;
+    margin-right: 10px;
+  }
 `;
 
 export const ListItem = styled.div`
   overflow: hidden;
-  padding:20px 0;
-  border-bottom:1px solid #dcdcdc;
+  padding: 20px 0;
+  border-bottom: 1px solid #dcdcdc;
+
   .pic {
     display: block;
     width: 125px;
@@ -67,19 +70,19 @@ export const ListItem = styled.div`
 export const ListInfo = styled.div`
   width: 500px;
   float: left;
-    .title {
-      font-size: 18px;
-      line-height: 1.5;
-      /* line-height: 27px; */
-      font-weight: bold;
-      color: #333;
-    }
 
-    .desc {
-      line-height: 24px;
-      font-size: 13px;
-      color: #777;
-    }
+  .title {
+    font-size: 18px;
+    line-height: 1.5;
+    font-weight: bold;
+    color: #333;
+  }
+
+  .desc {
+    line-height: 24px;
+    font-size: 13px;
+    color: #777;
+  }
 `
 
 export const LoadMore = styled.div`
@@ -99,12 +102,13 @@ export const BackTop = styled.div`
   right: 60px;
   z-index: 100;
   width: 70px;
-  height:50px;
+  height: 50px;
   line-height: 50px;
   text-align: center;
   font-size: 14px;
-  border-radius:10px;
+  border-radius: 10px;
   border: 1px solid #ccc;
   cursor: pointer;
+
   /* background-color: #ccc; */
 `
