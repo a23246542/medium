68206bfcf19e6a57e1ebefc88f486d2a92df2fe2
a23246542 <a23246542@gmail.com>
2021-04-20 23:51:06 +0800

    fix: header透明問題

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,11 +1,11 @@
-import React,{useState, useEffect,} from 'react';
-import {useSelector, useDispatch} from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { actionCreators as loginActionCreators } from '../../pages/login/store';
 import { CSSTransition } from 'react-transition-group';
 // import * as actionCreators from './store/actionCreators';
-import {actionCreators} from './store';
+import { actionCreators } from './store';
 // import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import {
   HeaderWrapper,
   Logo,
@@ -19,90 +19,91 @@ import {
   SearchInfoList,
   SearchInfoItem,
   Addition,
-  Button
+  Button,
 } from './style';
 
-
-
 const Header = () => {
-
   // const [focused,setFocused] = useState(false);
   // const focused = useSelector((state) => state.header.focused);
   // const focused = useSelector((state) => state.header.get('focused'));
-  const focused = useSelector((state) => state.getIn(['header','focused']));
-  const mouseIn = useSelector((state) => state.getIn(['header','mouseIn']));
-  const list = useSelector((state) => state.getIn(['header','list']));
-  const thisPage = useSelector((state) => state.getIn(['header','thisPage']));
-  const totalPages = useSelector((state) => state.getIn(['header','totalPages']));
-  const login = useSelector((state) => state.getIn(['login','isLogin']));
+  const focused = useSelector((state) => state.getIn(['header', 'focused']));
+  const mouseIn = useSelector((state) => state.getIn(['header', 'mouseIn']));
+  const list = useSelector((state) => state.getIn(['header', 'list']));
+  const thisPage = useSelector((state) => state.getIn(['header', 'thisPage']));
+  const totalPages = useSelector((state) =>
+    state.getIn(['header', 'totalPages'])
+  );
+  const login = useSelector((state) => state.getIn(['login', 'isLogin']));
 
   // ==============================================
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const fetchData = async () => await dispatch(actionCreators.getHotSearchList(list));
+    const fetchData = async () =>
+      await dispatch(actionCreators.getHotSearchList(list));
     fetchData();
-  },[])
+  }, []);
 
   // @@為何這邊也要async await
   const handleInputFocus = async (list) => {
     // setFocused(true);
-    (list.size === 0 ) &&  await dispatch(actionCreators.getHotSearchList(list));
+    list.size === 0 && (await dispatch(actionCreators.getHotSearchList(list)));
     dispatch(actionCreators.searchFocus());
-  }
+  };
   const handleInputBlur = () => {
     // setFocused(false);
     dispatch(actionCreators.searchBlur());
-  }
+  };
 
   const handleMouseEnter = () => {
     // dispatch(actionCreators.mouseIn(true))
-    dispatch(actionCreators.mouseEnter())
-  }
+    dispatch(actionCreators.mouseEnter());
+  };
 
   const handleMouseLeave = () => {
     // dispatch(actionCreators.mouseIn(false))
-    dispatch(actionCreators.mouseLeave())
-  }
+    dispatch(actionCreators.mouseLeave());
+  };
 
-
-  const handlePageChange = () => { // %%放在getListArea後面好像會找不到
+  const handlePageChange = () => {
+    // %%放在getListArea後面好像會找不到
     // if (thisPage!==totalPages){
-    if (thisPage < totalPages){
+    if (thisPage < totalPages) {
       dispatch(actionCreators.changePage(thisPage + 1));
     } else {
-      dispatch(actionCreators.changePage(1))
+      dispatch(actionCreators.changePage(1));
     }
-  }
+  };
 
-  const getListArea = () => { //渲染狀態focused mouseIn list
+  const getListArea = () => {
+    //渲染狀態focused mouseIn list
     const JsList = list.toJS(); // %%不可更改 list.toJs is not a function
     const pageList = [];
 
-    if(JsList.length) {
-      for(let i = (thisPage - 1) * 10 ; i < thisPage * 10 ; i++) {
+    if (JsList.length) {
+      for (let i = (thisPage - 1) * 10; i < thisPage * 10; i++) {
         // 第一頁0~小於10 第二頁10~小於20
         pageList.push(
-          <SearchInfoItem key={JsList[i]} >{JsList[i]}</SearchInfoItem> //JsList才能[i]
+          <SearchInfoItem key={JsList[i]}>{JsList[i]}</SearchInfoItem> //JsList才能[i]
         );
       }
     }
     // console.log(mouseIn);//!!一開始進來渲染一次 接著fouced改變渲染一次 等list資料回來又渲染一次
 
-    if ( focused || mouseIn ) {
+    if (focused || mouseIn) {
       return (
         <SearchInfo
-          onMouseEnter = { handleMouseEnter }
-          onMouseLeave = { handleMouseLeave }
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
         >
           <SearchInfoTitle>
             熱門搜索
-            <SearchInfoSwitch
-              onClick = { handlePageChange }
-            >換一換</SearchInfoSwitch>
+            <SearchInfoSwitch onClick={handlePageChange}>
+              換一換
+            </SearchInfoSwitch>
           </SearchInfoTitle>
           <SearchInfoList>
-            { pageList }
+            {pageList}
             {/* {list.map(item => (
               <SearchInfoItem key={item}>{item}</SearchInfoItem>
             ))} */}
@@ -111,64 +112,65 @@ const Header = () => {
             <SearchInfoItem>投稿</SearchInfoItem> */}
           </SearchInfoList>
         </SearchInfo>
-      )
+      );
     } else {
       return null;
     }
-  }
+  };
 
   const logout = () => {
     dispatch(loginActionCreators.logout());
-  }
+  };
 
   return (
     <HeaderWrapper>
       <Link to="/">
-        <Logo/>
+        <Logo />
       </Link>
       <Nav>
-          <NavItem className="left">首页</NavItem>
-          <NavItem className="left">下载APP</NavItem>
-          <NavItem className='right'>
-						<i className="iconfont">&#xe636;</i>
-					</NavItem>
-          <SearchWrapper>
-            {/* @@CSSTransition只能包一个如何影响到zoom */}
-            <CSSTransition
-							in={focused}
-							timeout={200}
-							classNames="slide"
-						>
-              <NavSearch
-                className={focused ? 'focused' :''}
-                onFocus={ () => { handleInputFocus(list) }}
-                onBlur={handleInputBlur}
-              ></NavSearch>
-            </CSSTransition>
-              {/* <i className="iconfont"
+        <NavItem className="left">首页</NavItem>
+        <NavItem className="left">下载APP</NavItem>
+        <NavItem className="right">
+          <i className="iconfont">&#xe636;</i>
+        </NavItem>
+        <SearchWrapper>
+          {/* @@CSSTransition只能包一个如何影响到zoom */}
+          <CSSTransition in={focused} timeout={200} classNames="slide">
+            <NavSearch
+              className={focused ? 'focused' : ''}
+              onFocus={() => {
+                handleInputFocus(list);
+              }}
+              onBlur={handleInputBlur}
+            ></NavSearch>
+          </CSSTransition>
+          {/* <i className="iconfont"
                 className={focused ? 'focused' :''} */}
-            <i className={focused? 'focused iconfont zoom' :'iconfont zoom'}
-            >&#xe614;</i>
-            { getListArea() }
-          </SearchWrapper>
+          <i className={focused ? 'focused iconfont zoom' : 'iconfont zoom'}>
+            &#xe614;
+          </i>
+          {getListArea()}
+        </SearchWrapper>
       </Nav>
       <Addition>
-        <Button className='writing'>写文章</Button>
-        {
-          login?
-          <Button className="reg" onClick={logout}>退出</Button>:
-          <Link to="/login"><Button className="reg">登陸</Button></Link>
-        }
+        <Button className="writing">写文章</Button>
+        {login ? (
+          <Button className="reg" onClick={logout}>
+            退出
+          </Button>
+        ) : (
+          <Link to="/login">
+            <Button className="reg">登陸</Button>
+          </Link>
+        )}
         {/* <Button className='reg'>登陸</Button> */}
       </Addition>
     </HeaderWrapper>
   );
 };
 
-
 // Header.propTypes = {
 
 // };
-
 
 export default Header;
--- src/common/header/style.js
+++ src/common/header/style.js
@@ -1,32 +1,31 @@
 import styled from 'styled-components/macro';
 import logoPic from '../../statics/logo.png';
 
-
 //padding-right70px可以推擠float
 // NavSearch如何擺放在中間
 // transition重包
 
-const border = 'border:1px solid #ccc'
+const border = 'border:1px solid #ccc';
 
 export const HeaderWrapper = styled.div`
-	z-index: 1;
-	position: relative;
-	height: 56px;
-	border-bottom: 1px solid #f0f0f0;
+  z-index: 1;
+  position: relative;
+  height: 56px;
+  border-bottom: 1px solid #f0f0f0;
 `;
 
 // export const Logo = styled.a.attrs({
 //   href:'/'
 // })`
 export const Logo = styled.div`
-	position: absolute;
-	top: 0;
-	left: 0;
-	display: block;
-	width: 100px;
-	height: 56px;
-	background: url(${logoPic});
-	background-size: contain;
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: block;
+  width: 100px;
+  height: 56px;
+  background: url(${logoPic});
+  background-size: contain;
 `;
 
 export const Nav = styled.div`
@@ -37,21 +36,21 @@ export const Nav = styled.div`
   box-sizing: border-box;
   margin: 0 auto;
   ${border};
-`
+`;
 
 export const NavItem = styled.div`
-	line-height: 56px;
-	padding: 0 15px;
-	font-size: 17px;
-	color: #333;
+  line-height: 56px;
+  padding: 0 15px;
+  font-size: 17px;
+  color: #333;
 
-	&.left {
-      float: left;
-    }
+  &.left {
+    float: left;
+  }
 
   &.right {
-      float: right;
-      color: #969696;
+    float: right;
+    color: #969696;
   }
 
   &.active {
@@ -62,74 +61,75 @@ export const NavItem = styled.div`
 // @@js zoom没有block依然能宽高
 // !!css 包外面靠父曾做法
 export const SearchWrapper = styled.div`
-	position: relative;
-	float: left;
+  position: relative;
+  float: left;
 
-	.zoom {
-		position: absolute;
+  .zoom {
+    position: absolute;
     right: 10px;
     bottom: 5px;
-		width: 30px;
-		line-height: 30px;
-		border-radius: 15px;
-		text-align: center;
-		&.focused {
-			background: #777;
-			color: #fff;
-		}
-	}
+    width: 30px;
+    line-height: 30px;
+    border-radius: 15px;
+    text-align: center;
+    &.focused {
+      background: #777;
+      color: #fff;
+    }
+  }
 `;
 
 export const NavSearch = styled.input.attrs({
-    placeholder: '搜索'
+  placeholder: '搜索',
 })`
-	width: 160px;
-	height: 38px;
-	padding: 0 30px 0 20px;
-	margin-top: 9px;
+  width: 160px;
+  height: 38px;
+  padding: 0 30px 0 20px;
+  margin-top: 9px;
   margin-left: 20px;
-	box-sizing: border-box;
-	border-radius: 19px;
-	border: none;
-	outline: none;
-	background: #eee;
-	font-size: 14px;
-	color: #666;
+  box-sizing: border-box;
+  border-radius: 19px;
+  border: none;
+  outline: none;
+  background: #eee;
+  font-size: 14px;
+  color: #666;
 
-	&::placeholder {
-		color: #999;
-	}
+  &::placeholder {
+    color: #999;
+  }
 
-	&.focused {
-		width: 240px;
-    }
+  &.focused {
+    width: 240px;
+  }
 
-	&.slide-enter {
-		transition: all .2s ease-out;
-	}
+  &.slide-enter {
+    transition: all 0.2s ease-out;
+  }
 
-	&.slide-enter-active {
-		width: 240px;
-	}
+  &.slide-enter-active {
+    width: 240px;
+  }
 
   &.slide-exit {
-		transition: all .2s ease-out;
-	}
+    transition: all 0.2s ease-out;
+  }
 
   &.slide-exit-active {
-		width: 160px;
-	}
+    width: 160px;
+  }
 `;
 
 export const SearchInfo = styled.div`
-  position:absolute;
+  position: absolute;
   left: 0;
   top: 56px;
   width: 240px;
   padding: 0 20px;
-  box-shadow: 0 0 9px 0 rgba(0,0,0,.2);
-  ${border}
-`
+  box-shadow: 0 0 9px 0 rgba(0, 0, 0, 0.2);
+  ${border};
+  background-color: #fff;
+`;
 
 export const SearchInfoTitle = styled.div`
   margin-top: 20px;
@@ -145,9 +145,8 @@ export const SearchInfoSwitch = styled.span`
   cursor: pointer;
 `;
 export const SearchInfoList = styled.div`
-	overflow: hidden;
+  overflow: hidden;
 `;
-
 
 export const SearchInfoItem = styled.a`
   display: block; /*寬高*/
@@ -155,36 +154,35 @@ export const SearchInfoItem = styled.a`
   line-height: 20px;
   padding: 0 5px;
   margin-right: 10px;
-	margin-bottom: 15px;
+  margin-bottom: 15px;
   font-size: 12px;
   border: 1px solid #ddd;
-  color:#787878;
+  color: #787878;
   border-radius: 3px;
 `;
 
 export const Addition = styled.div`
-	position: absolute;
-	right: 0;
-	top: 0;
-	height: 56px;
+  position: absolute;
+  right: 0;
+  top: 0;
+  height: 56px;
 `;
 
 export const Button = styled.div`
-	float: right;/*!!css 不一定要flex 不加换div占满宽*/
-	margin-top: 9px;
-	margin-right: 20px;
-	padding: 0 20px;
-	line-height: 38px;
-	border-radius: 19px;
-	border: 1px solid #ec6149;
-	font-size: 14px;
+  float: right; /*!!css 不一定要flex 不加换div占满宽*/
+  margin-top: 9px;
+  margin-right: 20px;
+  padding: 0 20px;
+  line-height: 38px;
+  border-radius: 19px;
+  border: 1px solid #ec6149;
+  font-size: 14px;
   cursor: pointer;
-	&.reg {
-		color: #ec6149;
-	}
-	&.writing {
-		color: #fff;
-		background: #ec6149;
-	}
-`
-
+  &.reg {
+    color: #ec6149;
+  }
+  &.writing {
+    color: #fff;
+    background: #ec6149;
+  }
+`;
