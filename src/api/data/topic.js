3d8d3989c98911a725f94efd2e9a7fdab7fb8e2c
a23246542 <a23246542@gmail.com>
2021-04-29 01:41:34 +0800

    feat: 文章更新(圖片)

--- src/api/data/topic.js
+++ src/api/data/topic.js
@@ -4,34 +4,46 @@ module.exports = {
     topicList: [
       {
         id: 1,
-        title: "社会热点",
-        imgUrl: "//upload.jianshu.io/collections/images/261938/man-hands-reading-boy-large.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64"
+        title: '社會熱點',
+        imgUrl:
+          '//upload.jianshu.io/collections/images/261938/man-hands-reading-boy-large.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64',
       },
       {
         id: 2,
-        title: "手手绘",
-        imgUrl: "//upload.jianshu.io/collections/images/21/20120316041115481.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64"
+        title: '生活感悟',
+        imgUrl:
+          '//upload.jianshu.io/collections/images/21/20120316041115481.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64',
       },
       {
         id: 3,
-        title: "社会热点",
-        imgUrl: "//upload.jianshu.io/collections/images/261938/man-hands-reading-boy-large.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64"
+        title: '日記本',
+        imgUrl:
+          '//upload.jianshu.io/collections/images/261938/man-hands-reading-boy-large.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64',
       },
       {
         id: 4,
-        title: "手手绘",
-        imgUrl: "//upload.jianshu.io/collections/images/21/20120316041115481.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64"
+        title: '連載文學',
+        imgUrl:
+          '//upload.jianshu.io/collections/images/21/20120316041115481.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64',
       },
       {
         id: 5,
-        title: "社会热点",
-        imgUrl: "//upload.jianshu.io/collections/images/261938/man-hands-reading-boy-large.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64"
+        title: '說到愛',
+        imgUrl:
+          '//upload.jianshu.io/collections/images/261938/man-hands-reading-boy-large.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64',
       },
       {
         id: 6,
-        title: "手手绘",
-        imgUrl: "//upload.jianshu.io/collections/images/21/20120316041115481.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64"
-      }
+        title: '雜談閒趣',
+        imgUrl:
+          '//upload.jianshu.io/collections/images/21/20120316041115481.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64',
+      },
+      {
+        id: 7,
+        title: '觀後有感',
+        imgUrl:
+          '//upload.jianshu.io/collections/images/21/20120316041115481.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64',
+      },
     ],
-  }
-}
+  },
+};
